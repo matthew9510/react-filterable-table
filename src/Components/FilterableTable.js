45ef318ce904e6739d217723e605cc2fd7f4bd6f
Matthew Hess <matthew9510@gmail.com>
2023-02-01 11:40:20 -0800

    Made Filterable table react to input state

--- src/Components/FilterableTable.js
+++ src/Components/FilterableTable.js
@@ -1,11 +1,27 @@
 import TableFilter from "./TableFilter";
 import FilteredTable from "./FilteredTable";
+import { useState } from "react";
 
 export default function FilterableTable({ products }) {
+    const [isInStockOnly, setIsInStockOnly] = useState(false);
+    const [tableSearchTerm, setTableSearchTerm] = useState('')
+    const filteredProducts = products.filter((product)=>{
+        if(product.name.toLowerCase().indexOf(tableSearchTerm.toLowerCase())!==-1){
+            if((isInStockOnly && product.stocked) || !isInStockOnly){
+                return true
+            }
+        }
+        return false
+    })
+
+    function onSearchTermChange(e){
+        setTableSearchTerm(e.target.value)
+    }
+
   return (
     <div className="filterable-table">
-      <TableFilter />
-      <FilteredTable products={products} />
+      <TableFilter isInStockOnly={isInStockOnly} setIsInStockOnly={setIsInStockOnly} tableSearchTerm={tableSearchTerm} onSearchTermChange={onSearchTermChange} />
+      <FilteredTable products={filteredProducts} />
     </div>
   );
 }
--- src/Components/TableFilter.js
+++ src/Components/TableFilter.js
@@ -1,10 +1,27 @@
-export default function TableFilter() {
+export default function TableFilter({
+  isInStockOnly,
+  setIsInStockOnly,
+  tableSearchTerm,
+  onSearchTermChange,
+}) {
   return (
     <div className="column-container">
-      <input type="text" placeholder="Search..." style={{marginBottom: 10}} />
+      <input
+        type="text"
+        placeholder="Search..."
+        style={{ marginBottom: 10 }}
+        value={tableSearchTerm}
+        onChange={onSearchTermChange}
+      />
       <label>
-        <input type="checkbox" />
-        {' '}
+        <input
+          type="checkbox"
+          value={isInStockOnly}
+          onChange={() => {
+            setIsInStockOnly((prevIsInStockOnly) => !prevIsInStockOnly);
+          }}
+        />
+        {" "}
         Only show products in stock
       </label>
     </div>
